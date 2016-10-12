9a13bc8b475a979de61aec92ea63d0215a865a47
Michał Ferchow <deseteral@gmail.com>
2016-10-13 00:20:04 +0200

    Added missing test

--- tests/components/expr-splash-screen/expr-splash-screen-spec.js
+++ tests/components/expr-splash-screen/expr-splash-screen-spec.js
@@ -1,12 +1,12 @@
 /* global chai */
 chai.should();
 
-describe('<expr-app>', () => {
+describe('<expr-splash-screen>', () => {
   let exprApp;
 
   beforeEach(() => {
     document.querySelector('#fixture').create();
-    exprApp = document.querySelector('expr-app');
+    exprApp = document.querySelector('expr-splash-screen');
   });
 
   afterEach(() => {
@@ -15,6 +15,6 @@ describe('<expr-app>', () => {
 
   it('should render title', () => {
     const title = exprApp.querySelector('h1').textContent;
-    title.should.eql('exprsjo');
+    title.should.eql('exprsjo splash screen');
   });
 });
