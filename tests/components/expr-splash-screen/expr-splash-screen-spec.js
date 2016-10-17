a2a918e6c28e65699b673174bc44d975f10badb8
Deseteral <deseteral@gmail.com>
2016-10-17 20:00:53 +0200

    List of recent library paths

--- tests/components/expr-splash-screen/expr-splash-screen-spec.js
+++ tests/components/expr-splash-screen/expr-splash-screen-spec.js
@@ -17,9 +17,18 @@ describe('<expr-splash-screen>', () => {
     expect(title).to.eql('exprsjo');
   });
 
-  it('should render subtitle', () => {
-    const subtitle = exprApp.querySelector('.subtitle').textContent;
-    expect(subtitle).to.eql('Open your photo library');
+  it('should render hints', () => {
+    const hints = [...exprApp.querySelectorAll('.subtitle')];
+    hints.map((hint) => hint.textContent);
+
+    const expectedHints = [
+      'Open your photo library',
+      'Open recent libraries'
+    ];
+
+    for (let i = 0; i < hints.length; i++) {
+      expect(hints[i]).to.eql(expectedHints[i]);
+    }
   });
 
   it('should render library path input inside container', () => {
@@ -30,6 +39,11 @@ describe('<expr-splash-screen>', () => {
   it('should render "browse" button inside container', () => {
     const button = libraryPathContainer().querySelector('.button-browse');
     expect(button).to.be.ok;
+  });
+
+  it('should render recent library paths list', () => {
+    const list = exprApp.querySelector('.list-recent');
+    expect(list).to.be.ok;
   });
 
   function libraryPathContainer() {
